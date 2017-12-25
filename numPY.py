f624141277ee2d22f2ff1e282b82f425b82d8670
mgh3326 <mgh3326@naver.com>
2017-12-25 09:45:51 +0900

    문자열 count

--- numPY.py
+++ numPY.py
@@ -1,7 +1,9 @@
 def numPY(s):
     # 함수를 완성하세요
+    if s.count('p') + s.count('P') == s.count('y') + s.count('Y'):
+        return True
 
-    return True
+    return False
 
 
 # 아래는 테스트로 출력해 보기 위한 코드입니다.
